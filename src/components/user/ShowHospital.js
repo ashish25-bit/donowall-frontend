bd75c0bc890b6f651c32658d9159b097d9a075f4
ashwin <sreaper37@gmail.com>
2020-10-29 18:54:37 +0300

    show hospital page added

--- src/components/user/ShowHospital.js
+++ src/components/user/ShowHospital.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import useTitle from '../../utils/useTitle';
-import { Link } from 'react-router-dom';
-import url from '../../utils/url';
+import React ,{useState,useEffect} from 'react';
 import Chart from '../admin/Chart'
 import userEvent from '@testing-library/user-event';
-import Hospital from '../../'
+import api from '../../utils/api'
+
 
 const ShowHospital=({match})=>{
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        async function getHospital() {
+            try {
+                const res = await api.get(`/user/hospital/${match.params.id}`);
+                console.log(res.data);
+                setData(res.data.hospital)
+                setLoading(false);
+            }
+            catch (err) {
+                console.log(err.response.data);
+            }
+        }
+
+        getHospital();
+    },[match.params.id]);
     return(
     <>
     <div className="profile-container d-flex justify-content-center">
-        <div className="show-container">
+        <div className="show-container ">
             <img src="https://www.bahrainspecialisthospital.com/images/about.png" alt=""/>
-            
-            <Chart data={}/>
-
+            <div className="d-flex justify-content-center hospital-name"><h3> {data.name}</h3></div>
+            <div className="graph-container">
+            {loading ?
+                ( <h1>...Loading</h1>)
+                : (data && <Chart data={data.blood_data
+                }/>)
+            }
+            </div><div className="d-flex justify-content-center">
+            <button className="book-btn btn">Book an appointment</button>
+            </div>
+                
+           
         </div>
         
 
--- src/utils/url.js
+++ src/utils/url.js
@@ -10,9 +10,7 @@ export default {
     userProfile: '/user/profile',
     userProfileEdit: '/user/profile/edit',
 
-<<<<<<< HEAD
-    showHospital:'/hospital/:id'
-=======
+    showHospital:'/user/hospital/:id',
+
     baseImageUrl: "https://api-donowall.herokuapp.com/api/admin/profile/photo"
->>>>>>> 06e8e69e905e2ecc287127855d2dc7bf987f9a45
 }
